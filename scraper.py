3f350985d5078d07f5ebb53872905d15f8db0e28
stevenfojas670 <84162024+stevenfojas670@users.noreply.github.com>
2025-04-20 22:57:24 -0700

    Removed main function that automatically runs the script

--- scraper.py
+++ scraper.py
@@ -10,10 +10,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
 
-username = os.environ.get('username')
-password = os.environ.get('password')
+username = os.environ.get("username")
+password = os.environ.get("password")
 
 cwd = os.getcwd()
+
 
 class Browser:
     browser, options = None, None
@@ -24,13 +25,13 @@ class Browser:
     def __init__(self, directory: str, download_flag: bool):
         self.options = webdriver.ChromeOptions()
         self.options.add_experimental_option("detach", True)
-        self.options.page_load_strategy = 'normal'
+        self.options.page_load_strategy = "normal"
         self.browser = webdriver.Chrome(options=self.options)
         self.request_data = {}
         self.directory = directory
         self.download_flag = download_flag
-        os.makedirs(self.directory, exist_ok=True) # Creating root directory
- 
+        os.makedirs(self.directory, exist_ok=True)  # Creating root directory
+
     def open_page(self, url: str):
         self.browser.get(url)
 
@@ -45,30 +46,34 @@ class Browser:
     def click_button(self, by: By, value: str):
         button = self.browser.find_element(by=by, value=value)
         wait = WebDriverWait(self.browser, timeout=2)
-        wait.until(lambda d : button.is_displayed())
+        wait.until(lambda d: button.is_displayed())
         actions = ActionChains(self.browser)
         actions.move_to_element(button).pause(2).click().perform()
 
-    def login_awsacademy(self, username: str, password: str):
+    def login(self, username: str, password: str):
         self.wait_random_time()
-        self.add_input(by=By.ID, value='pseudonym_session_unique_id', text=username)
+        self.add_input(by=By.ID, value="pseudonym_session_unique_id", text=username)
         self.wait_random_time()
-        self.add_input(by=By.ID, value='pseudonym_session_password', text=password)
+        self.add_input(by=By.ID, value="pseudonym_session_password", text=password)
         self.wait_random_time()
-        self.click_button(by=By.NAME, value='commit')
+        self.click_button(by=By.NAME, value="commit")
 
     def traverse_page(self):
         try:
-            
-            modules_container = self.browser.find_element(by=By.CLASS_NAME, value='ig-list')
-            module_sections = modules_container.find_elements(By.XPATH, ".//div[starts-with(@aria-label, 'Module')]")
+
+            modules_container = self.browser.find_element(
+                by=By.CLASS_NAME, value="ig-list"
+            )
+            module_sections = modules_container.find_elements(
+                By.XPATH, ".//div[starts-with(@aria-label, 'Module')]"
+            )
 
             for div in module_sections:
-                module_name = div.get_attribute('aria-label')
-                print(f'Processing: {module_name}')
-                content_div = div.find_element(by=By.CLASS_NAME, value='content')
-                modules_list = content_div.find_element(by=By.TAG_NAME, value='ul')
-                module_items = modules_list.find_elements(by=By.TAG_NAME, value='li')
+                module_name = div.get_attribute("aria-label")
+                print(f"Processing: {module_name}")
+                content_div = div.find_element(by=By.CLASS_NAME, value="content")
+                modules_list = content_div.find_element(by=By.TAG_NAME, value="ul")
+                module_items = modules_list.find_elements(by=By.TAG_NAME, value="li")
 
                 original_window = self.browser.current_window_handle
 
@@ -76,11 +81,16 @@ class Browser:
 
                 for item in module_items:
                     try:
-                        link_type_element = item.find_element(by=By.CLASS_NAME, value="type_icon")
-                        link_title = link_type_element.get_attribute('title')
+                        link_type_element = item.find_element(
+                            by=By.CLASS_NAME, value="type_icon"
+                        )
+                        link_title = link_type_element.get_attribute("title")
 
-                        if link_title == 'External Tool':
-                            link = item.find_element(by=By.XPATH, value=".//a[@class='ig-title title item_link']")
+                        if link_title == "External Tool":
+                            link = item.find_element(
+                                by=By.XPATH,
+                                value=".//a[@class='ig-title title item_link']",
+                            )
                             title = link.get_attribute("title")
                             second_page = link.get_attribute("href")
                             self.browser.execute_script("window.open()")
@@ -88,12 +98,16 @@ class Browser:
                             windows = self.browser.window_handles
                             self.browser.switch_to.window(windows[-1])
                             self.browser.get(second_page)
-                            self.get_request_information('https://emergingtalent.contentcontroller.com/vault', module_name, title)
+                            self.get_request_information(
+                                "https://emergingtalent.contentcontroller.com/vault",
+                                module_name,
+                                title,
+                            )
                             self.wait_random_time()
                             self.browser.close()
                             self.browser.switch_to.window(original_window)
                     except Exception as e:
-                        print(f'No link found in this {item.id}')
+                        print(f"No link found in this {item.id}")
 
         except Exception as e:
             print(f"An error occurred: {e}")
@@ -111,7 +125,7 @@ class Browser:
                         "directory": directory,
                         "url": request.url,
                         "headers": request.headers,
-                        "filename": filename
+                        "filename": filename,
                     }
                     del self.browser.requests
 
@@ -119,13 +133,13 @@ class Browser:
                         self.download()
 
                     return
-        
+
     def download(self):
 
-        url = self.request_data['url']
-        headers = self.request_data['headers']
-        name = self.request_data['filename']
-        directory = self.request_data['directory']
+        url = self.request_data["url"]
+        headers = self.request_data["headers"]
+        name = self.request_data["filename"]
+        directory = self.request_data["directory"]
 
         try:
             # Make the request
@@ -139,62 +153,76 @@ class Browser:
                 "application/pdf": ".pdf",
                 "text/plain": ".txt",
                 "application/json": ".json",
-            }.get(content_type, ".bin")  # Default to ".bin" if unknown
+            }.get(
+                content_type, ".bin"
+            )  # Default to ".bin" if unknown
 
-            if file_extension == '.bin':
-                file_extension = '.mp4'
+            if file_extension == ".bin":
+                file_extension = ".mp4"
 
             directory = self.remove_illegal_characters(directory)
             name = self.remove_illegal_characters(name)
 
             # Make directory titled after module name
-            os.makedirs(f'{self.directory}\\{directory}', exist_ok=True)
+            os.makedirs(f"{self.directory}\\{directory}", exist_ok=True)
 
             # Save to a file
-            with open(f'{cwd}\\{self.directory}\\{directory}\\{name}{file_extension}', "wb") as file:
+            with open(
+                f"{cwd}\\{self.directory}\\{directory}\\{name}{file_extension}", "wb"
+            ) as file:
                 for chunk in response.iter_content(chunk_size=8192):
                     file.write(chunk)
-            
-            print(f"File downloaded successfully as '{cwd}\\{self.directory}\\{directory}\\{name}\\{file_extension}'")
+
+            print(
+                f"File downloaded successfully as '{cwd}\\{self.directory}\\{directory}\\{name}\\{file_extension}'"
+            )
         except requests.exceptions.RequestException as e:
             print(f"An error occurred: {e}")
 
-    # Utility functions 
+    # Utility functions
     def wait_random_time(self):
         time.sleep(random.random())
 
     def find_specific_request(self, keyword: str):
         for request in self.browser.requests:
-            if request.method == 'GET' and keyword in request.url:
+            if request.method == "GET" and keyword in request.url:
                 print
 
     def remove_illegal_characters(self, input_string: str):
         # Define the regex pattern for illegal characters
         illegal_chars = r'[<>:"/\\|?*]'
-        
+
         # Replace illegal characters with an empty string
-        sanitized_string = re.sub(illegal_chars, '', input_string)
-        
+        sanitized_string = re.sub(illegal_chars, "", input_string)
+
         # Strip trailing spaces or periods
-        sanitized_string = sanitized_string.rstrip(' .')
-        
+        sanitized_string = sanitized_string.rstrip(" .")
+
         return sanitized_string
-    
+
+
 def run_multiple_drivers(directory: str, url: str, download_flag: bool):
-    print(f'{directory} thread starting!')
-    
+    print(f"{directory} thread starting!")
+
     browser = Browser(directory=directory, download_flag=download_flag)
-    browser.open_page('https://awsacademy.instructure.com/login/canvas')
-    browser.login_awsacademy(username, password)
+    browser.open_page("")
+    browser.login(username, password)
     time.sleep(8)
     browser.open_page(url)
     time.sleep(8)
     browser.traverse_page()
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
+    pass
     # Run thread with True to download files
-    foundations = threading.Thread(target=run_multiple_drivers, args=('AWS Cloud Foundations', 'https://awsacademy.instructure.com/courses/91362/modules', False))
-    foundations.start()
-    foundations.join()
+    # foundations = threading.Thread(
+    #     target=run_multiple_drivers,
+    #     args=(
+    #         "place_name_of_course_here",
+    #         "place_link_to_course_here",
+    #         False,
+    #     ),
+    # )
+    # foundations.start()
+    # foundations.join()
